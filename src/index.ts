99c54217cfc40eae35dc81038a10a29a98a512b6
Joshua Sebastian Kim <joshuakim150@gmail.com>
2024-07-15 21:26:51 -0300

    Add CORS and Cookie parser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express, { urlencoded, json } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import apiRouter from './api/router';
 import errorMiddleware from './middlewares/error';
 
-const port = process.env.PORT || 8000;
+const port = process.env.API_PORT || 8000;
 const app = express();
 
+app.use(
+  cors({
+    origin: ['http://localhost:8000'],
+    credentials: true,
+  }),
+);
+
+app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
